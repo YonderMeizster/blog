9fbcb20a858c66f3c5b2a9f83af99ca4c3176593
Denis <makrotheeast@gmail.com>
2023-05-03 21:10:23 +0300

    Changed view-func to view-classes

--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
 
-
-def index(request):
-
-    return render(request, "article/index.html", context={'app_name': f"{__package__.split('.')[1]}"})
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "article/index.html", context={
+            'app_name': f"{__package__.split('.')[1]}"
+            })
--- django_blog/views.py
+++ django_blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+class IndexView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html', context={
+            'who': 'World',
+        })
 
 
 def about(request):
