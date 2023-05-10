728f31137d5016577bc7c50facdaf1655626b488
Yonder_Meizster <makrotheeast@gmail.com>
2023-05-11 00:42:47 +0300

    Added update on articles

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -3,7 +3,8 @@ from django_blog.article import views
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(), name='articles'),
+    path('<int:id>/edit/', views.UpdateArticleView.as_view(), name='update_article'),
     path('<int:id>/', views.ArticleView.as_view(), name="article_id"),
     path('create/', views.CreateArticleView.as_view(), name="create_article"),
 ]
--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -16,16 +16,38 @@ class ArticleView(View):
         article = get_object_or_404(Article, id=kwargs['id'])
         return render(request, 'article/show.html', context={'article': article})
 
+
 class CreateArticleView(View):
     def get(self, request, *args, **kwargs):
         form = CreateArticle()
-        return render(request, 'article/create.html', context={'form' : form})
+        return render(request, 'article/create.html', context={'form': form})
 
     def post(self, request, *args, **kwargs):
         form = CreateArticle(request.POST)
-        
+
         if form.is_valid():
             form.save()
             return redirect(reverse('create_article'))
 
-        return render(request, 'article/create.html',  context={'form' : form})
+        return render(request, 'article/create.html',  context={'form': form})
+
+
+class UpdateArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = CreateArticle(instance=article)
+        return render(request, 'article/update.html', context={
+            'form': form,
+            'article_id': article_id})
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = CreateArticle(request.POST, instance=article)
+
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('articles'))
+
+        return render(request, 'articles/update.html', {'form' : form, 'article_id':article_id})
